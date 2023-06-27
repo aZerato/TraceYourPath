e0b8d8355c6c46b328c440cc36517e48d17b3f8c
aZerato <azerato.graphe@gmail.com>
2023-06-27 19:15:07 -0400

    FileUploaderCompo base

--- TraceYourPath/src/App.tsx
+++ TraceYourPath/src/App.tsx
@@ -1,10 +1,27 @@
+import React, { useState } from "react";
+import FileUploader from "./components/FileUploader/FileUploader";
 import './App.scss'
 
 function App() {
+  
+  const [selectedFile, setFile] = useState<File>();
+
+  const AppOnFileSelectSuccess = (file: File) =>
+  {
+    setFile(file);
+  };
 
   return (
     <>
       <h1>Hey</h1>
+      <form>
+      
+      <FileUploader
+          onFileSelectSuccess={AppOnFileSelectSuccess}
+          onFileSelectError={({ error }) => alert(error)}
+        />
+
+      </form>
     </>
   )
 }
---
+++ TraceYourPath/src/components/FileUploader/FileUploader.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent, useState } from 'react';
+
+interface FileUploaderProps
+{
+    onFileSelectSuccess: (file: File) => void,
+    onFileSelectError: (ex: any) => void
+}
+
+function FileUploader(Props: FileUploaderProps) 
+{
+  const [file, setFile] = useState<File>();
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    try 
+    {
+        if (e.target.files) 
+        {
+            setFile(e.target.files[0]);
+            Props.onFileSelectSuccess(e.target.files[0]);
+        }
+    }
+    catch(e: any)
+    {
+        Props.onFileSelectError(e);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+
+      <div>{file && `${file.name} - ${file.type}`}</div>
+    </div>
+  );
+}
+
+export default FileUploader;
